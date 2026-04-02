74a3cd917223edbe8e24a06d40790ebec71bf0ab
Yasser Lluberes <heinueljimenez07@gmail.com>
2026-04-02 19:51:53 -0400

    Configuracion final de PostgreSQL para persistencia

--- config/database.js
+++ config/database.js
@@ -1,9 +1,23 @@
-module.exports = ({ env }) => ({
-  connection: {
-    client: env('DATABASE_CLIENT', 'sqlite'),
-    connection: {
-      filename: env('DATABASE_FILENAME', '.tmp/data.db'),
-    },
-    useNullAsDefault: true,
-  },
-});
+const path = require('path');
+
+module.exports = ({ env }) => {
+  const connection = env('DATABASE_URL') 
+    ? {
+        client: 'postgres',
+        connection: {
+          connectionString: env('DATABASE_URL'),
+          ssl: env.bool('DATABASE_SSL', false), // Render lo maneja internamente
+        },
+      }
+    : {
+        client: 'sqlite',
+        connection: {
+          filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+        },
+        useNullAsDefault: true,
+      };
+
+  return {
+    connection
+  };
+};
